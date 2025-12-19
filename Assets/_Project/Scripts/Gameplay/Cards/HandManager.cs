ead670b5b676ae9c39481688d2e6d8463d6a46d7
DiamondApe95 <122873041+DiamondApe95@users.noreply.github.com>
2025-12-19 11:14:05 +0100

    Add minimal card deck and hand system

--- Assets/_Project/Scripts/Gameplay/Cards/HandManager.cs
+++ Assets/_Project/Scripts/Gameplay/Cards/HandManager.cs
@@ -1,16 +1,49 @@
-using UnityEngine;
+using System.Collections.Generic;
 
 namespace TowerOffense.Gameplay.Cards
 {
     public class HandManager
     {
-        public string Id { get; set; }
-        public bool IsEnabled { get; set; }
+        public List<string> hand = new();
+        public int handSize = 5;
 
-        public void Draw(int count)
+        public void FillToHandSize(DeckManager deck)
         {
-            UnityEngine.Debug.Log("Stub method called.");
+            if (deck == null)
+            {
+                return;
+            }
+
+            while (hand.Count < handSize)
+            {
+                string card = deck.DrawOne();
+                if (card == null)
+                {
+                    break;
+                }
+
+                hand.Add(card);
+            }
         }
 
+        public bool PlayCard(string cardId, DeckManager deck, CardPlayResolver resolver)
+        {
+            if (deck == null || resolver == null)
+            {
+                return false;
+            }
+
+            int index = hand.IndexOf(cardId);
+            if (index < 0)
+            {
+                return false;
+            }
+
+            hand.RemoveAt(index);
+            resolver.Play(cardId);
+            deck.Discard(cardId);
+            FillToHandSize(deck);
+            return true;
+        }
     }
 }
